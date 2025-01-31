8fdf31fbad69e45033fd5e8b447fca540bb2ca81
matheuswww <teteus.30.08.07@outlook.com>
2025-01-31 19:28:43 -0300

    checking if file is being processed in uploadFile

--- src/controller/upload/uploadFile.go
+++ src/controller/upload/uploadFile.go
@@ -1,7 +1,7 @@
 package upload_controller
 
 import (
-	"encoding/json"
+	"bytes"
 	"fmt"
 	"net/http"
 	"os"
@@ -14,7 +14,17 @@ import (
 	upload_controller_util "github.com/matheuswww/mystream/src/controller/upload/util"
 	"github.com/matheuswww/mystream/src/logger"
 	rest_err "github.com/matheuswww/mystream/src/restErr"
+	"github.com/matheuswww/mystream/src/router"
 )
+
+var beingProcessed map[string]bool = make(map[string]bool)
+
+func GetBeingProcessed(fileHash string) bool {
+	if _,v := beingProcessed[fileHash]; v {
+		return true
+	}
+	return false
+}
 
 func (uc *uploadController) UploadFile(w http.ResponseWriter, r *http.Request) {
 	logger.Log("Init UploadFile Controller")
@@ -33,8 +43,10 @@ func (uc *uploadController) UploadFile(w http.ResponseWriter, r *http.Request) {
 		conn.Close()
 		return
 	}
+	var fileHash string
 	var wg sync.WaitGroup
 	defer conn.Close()
+	defer delete(beingProcessed, fileHash)
 	for {
 		_, msg, err := conn.ReadMessage()
 		if err != nil {
@@ -42,7 +54,7 @@ func (uc *uploadController) UploadFile(w http.ResponseWriter, r *http.Request) {
 			break 
 		}
 		var uploadRequest upload_request.UploadFile
-		if err := json.Unmarshal(msg, &uploadRequest); err != nil {
+		if err := router.BindJson(bytes.NewReader(msg), &uploadRequest); err != nil {
 			logger.Error(fmt.Sprintf("Error trying Unmarshal: %v", err))
 			restErr := rest_err.NewBadRequestError("invalid fields")
 			upload_controller_util.SendWsRes(restErr, conn)
@@ -59,9 +71,13 @@ func (uc *uploadController) UploadFile(w http.ResponseWriter, r *http.Request) {
 			conn.Close()
 			break
 		}
+		if fileHash == "" {
+			beingProcessed[uploadRequest.FileHash] = true
+			fileHash = uploadRequest.FileHash
+		}
 		wg.Add(1)
 		go func() {
-			defer wg.Done()
+			wg.Done()
 			uc.uploadService.UploadFile(conn, uploadRequest)
 		}()
 	}

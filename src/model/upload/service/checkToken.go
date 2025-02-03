c75df71236c30ff1447ec9a793b8a7d8d78efe03
matheuswww <teteus.30.08.07@outlook.com>
2025-02-03 19:48:42 -0300

    parsing access token in upload CheckToken

--- src/model/upload/service/checkToken.go
+++ src/model/upload/service/checkToken.go
@@ -2,15 +2,14 @@ package upload_service
 
 import (
 	"fmt"
-
 	jwt_service "github.com/matheuswww/mystream/src/jwt"
 	"github.com/matheuswww/mystream/src/logger"
 )
 
-func (as *uploadService) CheckToken(refreshToken string) bool {
-	refreshClaims := jwt_service.ParseAdminRefreshToken(refreshToken)
+func (as *uploadService) CheckToken(token string) bool {
+	refreshClaims := jwt_service.ParseAdminAccessToken(token)
 	if refreshClaims == nil || refreshClaims.Valid() != nil {
-		logger.Error(fmt.Sprintf("Error trying ParseAdminRefreshToken: invalid claims"))
+		logger.Error(fmt.Sprintf("Error trying ParseAdminAccessToken: invalid claims"))
 		return false
 	}
 	return true
